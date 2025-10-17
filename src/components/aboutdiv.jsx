d6471a737cd05450c5f77f084638b213c3723587
Vickynina <vickynina006@gmail.com>
2025-10-18 04:36:07 +1300

    optimized speed

--- src/components/aboutdiv.jsx
+++ src/components/aboutdiv.jsx
@@ -1,4 +1,5 @@
 import arrow2 from "../assets/arrow2-icon.svg";
+import { motion } from "framer-motion";
 export default function AboutDIv({ img, title1, title2 }) {
   return (
     <div className="space-y-4 w-full font-semibold text-lg lg:w-[50%]">
@@ -16,11 +17,17 @@ export function SpanBar({ title }) {
   );
 }
 
-export function AboutButton({ title, img }) {
+export function AboutButton({ title, img, i }) {
   return (
-    <div className="flex  gap-2 py-3 px-6 rounded-full font-medium text-2xl bg-white outline outline-blacky outline-1">
+    <motion.div
+      initial={{ opacity: 0, y: 50, scale: 0.8 }}
+      whileInView={{ opacity: 1, y: 0, scale: 1 }}
+      viewport={{ amount: 0.3, once: "true" }}
+      transition={{ duration: 0.4, delay: i * 0.15 }}
+      className="flex  gap-2 py-3 px-6 rounded-full font-medium text-2xl bg-white outline outline-blacky outline-1"
+    >
       <img src={img} alt="" />
       <span>{title}</span>
-    </div>
+    </motion.div>
   );
 }
